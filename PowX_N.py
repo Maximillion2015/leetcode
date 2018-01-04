474afa4a2c1b9e4385f97e8c39b690c763bd2f6f
Maximillion2015 <15620602691@163.com>
2018-01-05 01:47:53 +0800

    50. Pow(x, n)

---
+++ PowX_N.py
@@ -0,0 +1,30 @@
+import time
+class Solution:
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+
+        if n == 0:
+            return 1
+        elif n > 0:
+            return self.myPowHelp(x, n)
+        else:
+            return 1 / self.myPowHelp(x, -n)
+
+    def myPowHelp(self, x, n):
+        if n == 1:
+            return x
+        elif n % 2 == 0:
+            return self.myPowHelp(x * x, n // 2)
+        else:
+            return x * self.myPowHelp(x * x, n // 2)
+
+if __name__ == '__main__':
+    time1 = time.time()
+    solution = Solution()
+    print(solution.myPow1(0.00001, 2147483647))
+    time2 = time.time()
+    print(time2-time1)
